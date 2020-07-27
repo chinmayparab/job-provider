06c62ee18cca3fcbb53f679067fc10f4e6390eee
rajsh <rajshah9172@gmail.com>
2020-07-27 22:51:09 +0530

    ek space ki kimat "where"

--- server/no_auth_apis.py
+++ server/no_auth_apis.py
@@ -18,11 +18,10 @@ def all_courses():
         cur = conn.cursor(pymysql.cursors.DictCursor)
         all = "Select * from courses"
         if(request.json['category'] != ""):
-            cur.execute(all+"WHERE category = '" +
+            cur.execute(all+" WHERE category = '" +
                         request.json['category']+"';")
         else:
             cur.execute(all+";")
-
         rows = cur.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
