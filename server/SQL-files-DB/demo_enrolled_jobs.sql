c244b35422de73b78dbe633049758d6d73b476f5
vikasxpandey <vikas.pandey792@gmail.com>
2020-08-01 11:57:34 +0530

    applicant-table

--- server/SQL-files-DB/demo_enrolled_jobs.sql
+++ server/SQL-files-DB/demo_enrolled_jobs.sql
@@ -30,7 +30,7 @@ CREATE TABLE `enrolled_jobs` (
   `question` varchar(2000) NOT NULL,
   `status` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
